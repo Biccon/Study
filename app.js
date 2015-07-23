16ff82245ea10cf7c772123b075450f353f14c2c
Biccon <admin@idx.kr>
2015-07-23 21:36:29 +0900

    로그인!

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// asdfasfasdf
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('test'));
+
+app.get('/login', function(req, res){
+	var username = req.query['username'];
+	var password = req.query['password'];
+
+	var result = {};
+
+	if(username == "admin" && password == "1234"){
+		result.status = "success";
+	} else {
+		result.status = "fail";
+	}
+	res.json(result);
+});
+
+app.listen(8080);
